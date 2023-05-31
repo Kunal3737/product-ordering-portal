cda83c0686c7c6688544cf901c071c26e644e563
Kunal <kunalshinde373@gmail.com>
2023-05-31 20:45:41 +0530

    Updated sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,23 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Sidebar.css";
-import { Link, NavLink } from "react-router-dom";
-const SIDEBAR_CONTENT = [
-  { label: "Dashboard", key: "dashboard" },
-  { label: "All Products", key: "all-products" },
-  { label: "Orders", key: "orders" },
-  { label: "Favorites", key: "favorites" },
-  { label: "New Arrival", key: "newArrival" },
-];
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
+  const [sidebarContent, setSidebarContent] = useState([
+    { label: "Dashboard", key: "dashboard", isActive: false },
+    { label: "All Products", key: "all-products", isActive: false },
+    { label: "Orders", key: "orders", isActive: false },
+    { label: "Favorites", key: "favorites", isActive: false },
+    { label: "New Arrival", key: "newArrival", isActive: false },
+  ]);
+
+  const handleLinkClick = (item) => {
+    let dummySidebarContent = sidebarContent.filter((el) => {
+      if (item.key === el.key) {
+        return {
+          label: item.label,
+          key: item.key,
+          isActive: true,
+        };
+      }
+
+      return el;
+    });
+
+    setSidebarContent(dummySidebarContent);
+  };
+
   return (
     <div className="sidebar-wrapper">
       <div className="sidebar-logo sidebar-logo-wrapper">
         <h2>Logo</h2>
       </div>
 
-      {SIDEBAR_CONTENT.map((item) => (
-        <NavLink to={"/" + item.key} className="sidebar-content">
+      {sidebarContent.map((item) => (
+        <NavLink
+          onClick={() => handleLinkClick(item)}
+          to={"/" + item.key}
+          className={`sidebar-content ${item.isActive ? "active" : ""}`}
+        >
           {item.label}
         </NavLink>
       ))}
